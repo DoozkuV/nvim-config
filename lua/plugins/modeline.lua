2d280313773860f6db86119b1344ff78d93727ee
George Padron <georgenpadron@gmail.com>
2023-11-08 17:49:27 +0100

    Added symbol files, lualine re-enabled, telescope

--- lua/plugins/modeline.lua
+++ lua/plugins/modeline.lua
@@ -10,5 +10,5 @@ return {
         section_separators = '',
       },
     },
-    enabled = false,
+    -- enabled = false,
   }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,43 +20,57 @@ return {
   ft = 'mason',
   keys = {
 
-    { '<leader>sr', function() require('telescope.builtin').oldfiles() end, {
+    {
+      '<leader>sr',
+      function() require('telescope.builtin').oldfiles() end,
       desc =
       '[S]earch [R]ecently opened files'
-    } },
-    { '<leader>,',  function() require('telescope.builtin').buffers() end,    { desc = '[,] Find existing buffers' } },
-    { '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' } },
-    { '<leader>gf', function() require('telescope.builtin').git_files() end,  { desc = 'Search [G]it [F]iles' } },
-    { '<leader> ',  function() require('telescope.builtin').find_files() end, { desc = '[ ] Find Files' } },
-    { '<leader>sh', function() require('telescope.builtin').help_tags() end,  { desc = '[S]earch [H]elp' } },
+    },
+    { '<leader>,',  function() require('telescope.builtin').buffers() end,    desc = '[,] Find existing buffers' },
+    {
+      '<leader>/',
+      function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end,
+      desc = '[/] Fuzzily search in current buffer'
+    },
+    { '<leader>gf', function() require('telescope.builtin').git_files() end,  desc = 'Search [G]it [F]iles' },
+    { '<leader> ',  function() require('telescope.builtin').find_files() end, desc = '[ ] Find Files' },
+    { '<leader>sh', function() require('telescope.builtin').help_tags() end,  desc = '[S]earch [H]elp' },
     { '<leader>sw', function()
       require('telescope.builtin').grep_string()
     end, { desc = '[S]earch current [W]ord' } },
-    { '<leader>sg', function() require('telescope.builtin').live_grep() end,      { desc = '[S]earch by [G]rep' } },
-    { '<leader>sd', function() require('telescope.builtin').diagnostics() end, {
+    { '<leader>sg', function() require('telescope.builtin').live_grep() end,      desc = '[S]earch by [G]rep' },
+    {
+      '<leader>sd',
+      function() require('telescope.builtin').diagnostics() end,
       desc =
       '[S]earch [D]iagnostics'
-    } },
-    { '<leader>sf', function() require('telescope.builtin').find_files() end,     { desc = '[S]earch [F]iles' } },
+    },
+    { '<leader>sf', function() require('telescope.builtin').find_files() end,     desc = '[S]earch [F]iles' },
+
+    { '<leader>sm', function() require('telescope.builtin').man_pages() end,      desc = '[S]earch [M]an' },
 
     -- LSP Keybinds
-    { 'gr',         function() require('telescope.builtin').lsp_references() end, { desc = '[G]oto [R]eferences' } },
-    { '<leader>ds', function() require('telescope.builtin').lsp_document_symbols() end,
-      {
-        desc =
-        '[D]ocument [S]ymbols'
-      } },
-    { '<leader>ws', function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end,
-      {
-        desc =
-        '[W]orkspace [S]ymbols'
-      } },
+    { 'gr',         function() require('telescope.builtin').lsp_references() end, desc = '[G]oto [R]eferences' },
+    {
+      '<leader>ds',
+      function() require('telescope.builtin').lsp_document_symbols() end,
+
+      desc =
+      '[D]ocument [S]ymbols'
+    },
+    {
+      '<leader>ws',
+      function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end,
+
+      desc =
+      '[W]orkspace [S]ymbols'
+    },
   },
   config = function()
     require('telescope').setup {
