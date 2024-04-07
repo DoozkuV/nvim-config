3a6563481b47432b65941243268b3fc266582457
George Padron <georgenpadron@gmail.com>
2024-04-06 21:55:30 -0500

    Conjure configuration

---
+++ lua/plugins/conjure.lua
@@ -0,0 +1,34 @@
+-- Conjure enables REPL like functionality in NVIM, like Emacs
+return {
+  "Olical/conjure",
+  ft = { "clojure", "fennel", "lua" },
+  cmd = { "ConjureSchool" },
+  -- cmp integration
+  dependencies = {
+    {
+      "PaterJason/cmp-conjure",
+      config = function()
+        local cmp = require("cmp")
+        local config = cmp.get_config()
+        table.insert(config.sources, {
+          name = "buffer",
+          option = {
+            sources = {
+              { name = "conjure" },
+            },
+          },
+        })
+        cmp.setup(config)
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("conjure.main").main()
+    require("conjure.mapping")["on-filetype"]()
+  end,
+  -- Set configuration options here
+  init = function()
+    -- Documentation prefix
+    vim.g["conjure#mapping#doc_word"] = "<C-k>"
+  end
+}
