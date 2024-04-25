82eee7f98376f5b47269dd669765415e9485700e
George Padron <georgenpadron@gmail.com>
2024-04-25 18:47:19 -0500

    Toggleable completion and otter (disabled)

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,4 +1,7 @@
+-- Global variable to enable/disable completion
+vim.g.cmp_toggle_flag = true
 -- Plugins relating to completion in nvim
+local otter_ft = { 'markdown', 'html', 'nix' }
 return {
   -- Autocompletion
   'hrsh7th/nvim-cmp',
@@ -15,7 +18,24 @@ return {
 
     -- Adds completion to the cmdline
     'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-buffer'
+    'hrsh7th/cmp-buffer',
+
+    -- Enables LSP Inside of Codeblocks
+    {
+      'jmbuhr/otter.nvim',
+      enabled = false,
+      ft = otter_ft,
+      config = function()
+        local otter_languages = { 'python', 'lua', 'bash', 'sh', 'javascript', 'css' }
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = otter_ft,
+          callback = function()
+            require("otter").activate(otter_languages, true, true)
+          end,
+        })
+      end
+    },
+
   },
   event = { 'InsertEnter', 'CmdlineEnter' },
   config = function()
@@ -68,6 +88,10 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+      -- Enable or disable completion
+      enabled = function()
+        return vim.g.cmp_toggle_flag
+      end,
       mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -88,6 +112,7 @@ return {
         { name = 'luasnip' },
         -- Commented this out as well - see neorg.lua
         -- { name = 'neorg' },
+        { name = 'otter' },
       },
     }
 
@@ -142,4 +167,15 @@ return {
       })
     })
   end,
+  keys = {
+    { -- Toggle completion binding
+      '<leader>tc',
+      function()
+        vim.g.cmp_toggle_flag = not vim.g.cmp_toggle_flag
+        print("Completion "
+          .. (vim.g.cmp_toggle_flag and "enabled" or "disabled"))
+      end,
+      desc = '[T]oggle [C]ompletion',
+    }
+  },
 }
