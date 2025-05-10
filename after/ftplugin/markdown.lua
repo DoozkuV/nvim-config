d2bf89ecf1b5135390ceb855f69bf9922b4875ed
George Padron <georgenpadron@gmail.com>
2025-05-10 18:20:10 -0500

    Updated markdown bold/italics commands

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,31 @@
+--- Inserts the delimiter and places the cursor in the middle. If already inside the delimiter, pushes the cursor out
+---@param del string
+local function insert_delimiters(del)
+    local line = vim.api.nvim_get_current_line()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local row, col = cursor_pos[1], cursor_pos[2]
+
+    -- Check if the character after cursor is an asterisk
+    if col < #line - #del + 1 and string.sub(line, col + 1, col + #del) == del then
+        -- Move cursor after asterisk
+        vim.api.nvim_win_set_cursor(0, { row, col + #del })
+    else
+        -- Insert asterisk and place cursor between them
+        local new_line = string.sub(line, 1, col) .. del .. del .. string.sub(line, col + 1)
+        vim.api.nvim_set_current_line(new_line)
+        vim.api.nvim_win_set_cursor(0, { row, col + #del })
+    end
+end
+
+
+-- Set them keybinds
+local keymap_opts = { noremap = true, buffer = true }
+vim.keymap.set('i', '<C-i>', function()
+    insert_delimiters('*')
+end, keymap_opts)
+vim.keymap.set('i', '<C-b>', function()
+    insert_delimiters('**')
+end, keymap_opts)
+
 -- Enable spellchecking automatically in markdown buffers
 vim.opt_local.spell = true
-
--- Create keybinds for italicizing and bolding text
-vim.api.nvim_buf_set_keymap(0, 'i', '<C-i>', '**<ESC>i', { silent = true, noremap = true })
-vim.api.nvim_buf_set_keymap(0, 'i', '<C-b>', '****<ESC>hi', { silent = true, noremap = true })
