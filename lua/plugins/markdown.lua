b385b90fbff05ed6ba83ae775acb7d515408e616
George Padron <georgenpadron@gmail.com>
2026-01-07 13:05:08 -0500

    Updated markdown preview plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,32 +1,5 @@
-local function toggle()
-  local peek = require("peek")
-  if peek.is_open() then
-    print("Closing Peek")
-    peek.close()
-  else
-    print("Opening Peek")
-    peek.open()
-  end
-end
-
 return {
-  'toppair/peek.nvim',
-  event = { "VeryLazy" },
-  build = 'deno task --quiet build:fast',
-  keys = {
-    {
-      "<leader>oP",
-      toggle,
-      desc = "Markdown Preview Toggle"
-    }
-  },
-  opts = {
-    auto_load = false,
-    close_on_bdelete = true,
-    syntax = true,
-    theme = "dark",
-    update_on_change = true,
-    app = "webview",
-    filetype = { "markdown" },
-  }
+  'brianhuster/live-preview.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim' },
+  ft = { 'markdown' },
 }
